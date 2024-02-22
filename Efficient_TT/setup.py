b9470f64f878a21764b4f0d9066991e6dfe8dd0f
Shenghao <1249646094@qq.com>
2024-02-22 14:16:27 +0000

    An efficient TTEmbeddingBag in cuda

---
+++ Efficient_TT/setup.py
@@ -0,0 +1,40 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from setuptools import find_packages, setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+tnn_utils = CUDAExtension(
+    name="effi_tt_embeddings",
+    sources=[
+        "efficient_kernel_wrap.cpp",
+        "efficient_tt_cuda.cu",
+    ],
+    extra_compile_args={
+        "cxx": [
+            "-O3",
+            "-g",
+            "-DUSE_MKL",
+            "-m64",
+            "-mfma",
+            "-masm=intel",
+        ],
+        "nvcc": [
+            "-O3",
+            "-g",
+            "--expt-relaxed-constexpr",
+            "-D__CUDA_NO_HALF_OPERATORS__",
+            "-I/home/shenghao/cub-2.1.0",
+            '-gencode=arch=compute_86,code="sm_86"',
+        ],
+    },
+)
+setup(
+    name="effi_tt_embeddings",
+    description="effi_tt_embeddings",
+    packages=find_packages(),
+    ext_modules=[tnn_utils],
+    cmdclass={"build_ext": BuildExtension},
+)
